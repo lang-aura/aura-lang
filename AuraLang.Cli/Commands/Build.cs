7edb7717699e60cb81d6603d46b7345751d11073
Owen Hamill <47223204+ohamill@users.noreply.github.com>
2023-11-09 20:27:56 +0100

    Start CLI (#1)

---
+++ AuraLang.Cli/Commands/Build.cs
@@ -0,0 +1,13 @@
+﻿using AuraLang.Cli.Options;
+
+namespace AuraLang.Cli.Commands;
+
+public static class Build
+{
+	public static int ExecuteBuild(BuildOptions opts)
+	{
+		Console.WriteLine($"Executing build with verbose = {opts.Verbose}");
+		return 0;
+	}
+}
+
---
+++ AuraLang.Cli/Commands/Run.cs
@@ -0,0 +1,13 @@
+﻿using AuraLang.Cli.Options;
+
+namespace AuraLang.Cli.Commands;
+
+public static class Run
+{
+	public static int ExecuteRun(RunOptions opts)
+	{
+		Console.WriteLine($"Executing run with verbose = {opts.Verbose}");
+		return 0;
+	}
+}
+
