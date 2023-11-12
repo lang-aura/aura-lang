c09c3baa0067c1f233354ae8d1ea7e617a6aaad7
Owen Hamill <47223204+ohamill@users.noreply.github.com>
2023-11-12 21:08:18 +0100

    Create compiler (#5)

---
+++ AuraLang/Compiler/StringBuilder.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using AuraLang.AST;
+
+namespace AuraLang.Compiler;
+
+/// <summary>
+/// Compiles a section of an Aura source to Go. The compilation process tracks three different sections (<c>pkg</c>, <c>imports</c>,
+/// <c>statements</c>), compiling a separate string for each section and then combining them at the end.
+/// </summary>
+public class AuraStringBuilder
+{
+    private StringBuilder sb;
+    private int lastLine = 0;
+
+    public AuraStringBuilder()
+    {
+        sb = new(string.Empty);
+    }
+
+	public AuraStringBuilder(string s)
+    {
+        sb = new(s);
+    }
+
+    public void WriteString(string s, int line, TypedAuraStatement typ)
+    {
+        // A value of 0 for `lastLine` indicates that no string has previously been written to this struct
+        if (lastLine == 0)
+        {
+            sb.Append(s);
+            lastLine = line;
+        }
+        // If `lastLine` is less than the current string's line, we prepend a newlin to the string before writing it to the string builder
+        else if (lastLine < line)
+        {
+            sb.Append($"\n{s}");
+            lastLine = line;
+        }
+        else
+        {
+            // If the current string will be written to the same line as the previous string, we separate the two strings with either a space
+            // (if the new string is a comment), or an explicit semicolon
+            if (typ is TypedComment)
+            {
+                sb.Append($" {s}");
+            }
+            else
+            {
+                sb.Append($"; {s}");
+            }
+        }
+    }
+
+    public string String() => sb.ToString();
+}
---
+++ AuraLang/Exceptions/Compiler/Compiler.cs
@@ -0,0 +1,16 @@
+﻿namespace AuraLang.Exceptions.Compiler;
+
+public abstract class CompilerException : AuraException
+{
+    public CompilerException(string message, int line) : base(message, line) { }
+}
+
+public class UnknownStatementException : CompilerException
+{
+    public UnknownStatementException(int line) : base("Unknown statement", line) { }
+}
+
+public class UnknownExpressionException : CompilerException
+{
+    public UnknownExpressionException(int line) : base("Unknown expression", line) { }
+}
