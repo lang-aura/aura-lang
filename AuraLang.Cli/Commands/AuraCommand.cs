317a8eae58b030d7f64aeaf7a7e377caef89da48
Owen Hamill <owen.hamill@devoteam.com>
2023-12-08 12:57:11 +0100

    Add comments to CLI package

--- AuraLang.Cli/Commands/AuraCommand.cs
+++ AuraLang.Cli/Commands/AuraCommand.cs
@@ -4,6 +4,9 @@ namespace AuraLang.Cli.Commands;
 
 public abstract class AuraCommand
 {
+	/// <summary>
+	/// Indicates whether the command's output is verbose
+	/// </summary>
 	protected bool Verbose { get; init; }
 
 	protected AuraCommand(AuraOptions opts)
@@ -12,14 +15,27 @@ public abstract class AuraCommand
 	}
 	public abstract int Execute();
 
+	/// <summary>
+	/// Traverses the project's Aura source files and calls the supplied Action on each file
+	///	</summary>
+	/// <param name="a">The Action to call on each Aura source file in the project. The action will accept
+	/// each file's path as a parameter</param>
 	protected void TraverseProject(Action<string> a)
 	{
 		TraverseProjectRecur("./src", a);
 	}
 
+	/// <summary>
+	/// Recursively traverses the current directory and any sub-directories, calling the supplied Action
+	/// on each Aura source file
+	/// </summary>
+	/// <param name="path">The path of the current directory being traversed. All Aura source files located in the current
+	/// directory and any sub-directories will be processed by the supplied Action</param>
+	/// <param name="a">The Action to call on each Aura source file in the current directory and any sub-directories. The action will
+	/// accept each file's path as a parameter</param>
 	private void TraverseProjectRecur(string path, Action<string> a)
 	{
-		var paths = Directory.GetFiles(path);
+		var paths = Directory.GetFiles(path, "*.aura");
 		foreach (var p in paths)
 		{
 			a(p);
--- AuraLang.Cli/Commands/Fmt.cs
+++ AuraLang.Cli/Commands/Fmt.cs
@@ -4,15 +4,22 @@ namespace AuraLang.Cli.Commands;
 
 public class Fmt : AuraCommand
 {
-
 	public Fmt(FmtOptions opts) : base(opts) { }
 
+	/// <summary>
+	/// Formats the entire Aura project
+	/// </summary>
+	/// <returns>An integer status indicating if the command succeeded</returns>
 	public override int Execute()
 	{
 		TraverseProject(FormatFile);
 		return 0;
 	}
 
+	/// <summary>
+	/// Formats an individual Aura source file
+	/// </summary>
+	/// <param name="path">The path of the Aura source file</param>
 	public void FormatFile(string path)
 	{
 		var contents = File.ReadAllText(path);
