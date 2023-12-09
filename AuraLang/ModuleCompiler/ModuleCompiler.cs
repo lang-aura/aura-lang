d4824e28438f5885e8fe7ecab541550c5c0b2a8d
Owen Hamill <owen.hamill@devoteam.com>
2023-12-09 21:08:30 +0100

    Add compiler hierarchy

---
+++ AuraLang/ModuleCompiler/ModuleCompiler.cs
@@ -0,0 +1,39 @@
+using AuraLang.AST;
+using AuraLang.FileCompiler;
+using AuraLang.TypeChecker;
+
+namespace AuraLang.ModuleCompiler;
+
+public class AuraModuleCompiler
+{
+	/// <summary>
+	/// The path of the module
+	/// </summary>
+	private string Path { get; }
+
+	private string ProjectName { get; }
+
+	/// <summary>
+	/// The same type checker will be used for all Aura source files in the module
+	/// </summary>
+	private AuraTypeChecker TypeChecker { get; }
+
+	public AuraModuleCompiler(string path, string projectName)
+	{
+		Path = path;
+		ProjectName = projectName;
+		TypeChecker = new AuraTypeChecker(
+			new VariableStore(),
+			new EnclosingClassStore(),
+			new EnclosingNodeStore<IUntypedAuraExpression>(),
+			new EnclosingNodeStore<IUntypedAuraStatement>(),
+			new LocalModuleReader());
+	}
+
+	public List<string> CompileModule()
+	{
+		return Directory.GetFiles(Path, "*.aura")
+			.Select(p => new AuraFileCompiler(p, ProjectName, TypeChecker).CompileFile())
+			.ToList();
+	}
+}
