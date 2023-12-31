c7a7223d0f339cf56bf849c01b2a945778f6c392
Owen Hamill <47223204+ohamill@users.noreply.github.com>
2023-12-31 17:39:12 -0500

    Add `warnings-as-errors` CLI flag (#124)
    * Add `warnings-as-errors` CLI flag
    
    * Linting

--- AuraLang.Cli/Options/Build.cs
+++ AuraLang.Cli/Options/Build.cs
@@ -3,5 +3,8 @@
 namespace AuraLang.Cli.Options;
 
 [Verb("build", HelpText = "Compile the Aura project")]
-public class BuildOptions : AuraOptions { }
-
+public class BuildOptions : AuraOptions
+{
+	[Option("warnings-as-errors", Required = false, Default = false, HelpText = "Treats warnings as errors")]
+	public bool WarningsAsErrors { get; set; }
+}
