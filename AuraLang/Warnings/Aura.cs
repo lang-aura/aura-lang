5517f5f8832fed379a168b2f535b4a061c10b04e
Owen Hamill <47223204+ohamill@users.noreply.github.com>
2023-12-31 17:00:55 -0500

    Add warnings (#121)

---
+++ AuraLang/Warnings/Aura.cs
@@ -0,0 +1,33 @@
+﻿namespace AuraLang;
+
+public abstract class AuraWarningContainer : Exception
+{
+	public readonly List<AuraWarning> Warnings = new();
+	protected string FilePath { get; init; }
+
+	public AuraWarningContainer(string filePath)
+	{
+		FilePath = filePath;
+	}
+
+	public bool IsEmpty() => Warnings.Count == 0;
+
+	public void Report()
+	{
+		var warnings = Warnings.Select(w => w.Warn(FilePath));
+		var output = string.Join("\n\n", warnings);
+		Console.WriteLine(output);
+	}
+}
+
+public abstract class AuraWarning : Exception
+{
+	public int Line { get; }
+
+	protected AuraWarning(string message, int line) : base(message)
+	{
+		Line = line;
+	}
+
+	public string Warn(string filePath) => $"[{filePath} line {Line}] {Message}";
+}
