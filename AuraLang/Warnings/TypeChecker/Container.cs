e05a1b041900576f4a7c2d53e524990da6305d4e
Owen Hamill <47223204+ohamill@users.noreply.github.com>
2023-12-31 17:23:53 -0500

    Unused variable warning (#123)
    * Add unused variable warning
    
    * Linting

---
+++ AuraLang/Warnings/TypeChecker/Container.cs
@@ -0,0 +1,11 @@
+﻿namespace AuraLang;
+
+public class TypeCheckerWarningContainer : AuraWarningContainer
+{
+	public TypeCheckerWarningContainer(string filePath) : base(filePath) { }
+
+	public void Add(TypeCheckerWarning w)
+	{
+		Warnings.Add(w);
+	}
+}
---
+++ AuraLang/Warnings/TypeChecker/TypeChecker.cs
@@ -0,0 +1,11 @@
+﻿namespace AuraLang;
+
+public class TypeCheckerWarning : AuraWarning
+{
+	protected TypeCheckerWarning(string message, int line) : base(message, line) { }
+}
+
+public class UnusedVariableWarning : TypeCheckerWarning
+{
+	public UnusedVariableWarning(string varName, int line) : base($"Variable {varName} not used", line) { }
+}
