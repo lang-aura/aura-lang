6fa5f38d9a89b2650e20228d7bea9df3f0de690c
Owen Hamill <47223204+ohamill@users.noreply.github.com>
2024-01-13 16:09:12 +0100

    Move Aura executable to different directory (#135)

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 dotnet publish AuraLang.Cli -r osx-x64 --self-contained
 mv ./AuraLang.Cli/bin/Debug/net7.0/osx-x64/publish/AuraLang.Cli ./AuraLang.Cli/bin/Debug/net7.0/osx-x64/publish/aura
-sudo mv ./AuraLang.Cli/bin/Debug/net7.0/osx-x64/publish/aura /usr/local/bin
+mv ./AuraLang.Cli/bin/Debug/net7.0/osx-x64/publish/aura "${HOME}/.aura"
