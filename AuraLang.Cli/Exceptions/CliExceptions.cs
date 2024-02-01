280fe2e72e47b99a4a9e6e60a55d13542bbd1172
Owen Hamill <47223204+ohamill@users.noreply.github.com>
2024-02-01 10:33:22 +0100

    Ensure new parent directory is empty (#166)

--- AuraLang.Cli/Exceptions/CliExceptions.cs
+++ AuraLang.Cli/Exceptions/CliExceptions.cs
@@ -4,3 +4,8 @@ public class TomlFileNotFoundException : Exception
 {
 	public TomlFileNotFoundException() : base("aura.toml file not found in current directory or any parent directories.") { }
 }
+
+public class NewParentDirectoryMustBeEmpty : Exception
+{
+	public NewParentDirectoryMustBeEmpty() : base("The project's parent directory must be empty.") { }
+}
