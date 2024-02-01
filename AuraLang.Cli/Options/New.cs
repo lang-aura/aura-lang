51b326b83871c77a0bea5157d23b1850ae6c7290
Owen Hamill <owen.hamill@devoteam.com>
2024-02-01 10:19:28 +0100

    Support `output` option for `aura new` command

--- AuraLang.Cli/Options/New.cs
+++ AuraLang.Cli/Options/New.cs
@@ -7,4 +7,6 @@ public class NewOptions : AuraOptions
 {
 	[Value(0, Required = true, HelpText = "The name of the new Aura project")]
 	public string? Name { get; set; }
+	[Option('o', "output", Required = false, HelpText = "The directory where the new Aura project will be created")]
+	public string? OutputPath { get; set; }
 }
