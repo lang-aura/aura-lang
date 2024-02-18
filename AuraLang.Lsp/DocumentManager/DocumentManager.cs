e80ec9495fef76c4f73fe46d42693663204ff7a0
Owen Hamill <owen.hamill@devoteam.com>
2024-02-18 08:48:36 +0100

    Store typed AST in LSP server

--- AuraLang.Lsp/DocumentManager/DocumentManager.cs
+++ AuraLang.Lsp/DocumentManager/DocumentManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Concurrent;
 using AuraLang.Exceptions;
+using AuraLang.Lsp.Document;
 using AuraLang.Lsp.SynchronizedFileProvider;
 using AuraLang.Parser;
 using AuraLang.Scanner;
@@ -10,19 +11,14 @@ namespace AuraLang.Lsp.DocumentManager;
 
 public class AuraDocumentManager
 {
-	private readonly ConcurrentDictionary<string, ConcurrentDictionary<string, string>> _documents = new();
+	private readonly ConcurrentDictionary<string, ConcurrentDictionary<string, AuraLspDocument>> _documents = new();
 
 	public void UpdateDocument(string path, string contents)
 	{
 		var (module, file) = GetModuleAndFileNames(path);
-		// Store file's new contents
-		var modDict = _documents.GetOrAdd(module, (_) => new ConcurrentDictionary<string, string>());
-		modDict.AddOrUpdate(file, contents, (k, v) => v);
-		_documents.AddOrUpdate(module, modDict, (k, dict) => dict);
-		_documents[module] = modDict;
-		// Compile file's new contents
 		try
 		{
+			// Compile file's new contents
 			var tokens = new AuraScanner(contents, path).ScanTokens().Where(tok => tok.Typ is not TokType.Newline).ToList();
 			var untypedAst = new AuraParser(tokens, path).Parse();
 			var typeChecker = new AuraTypeChecker(
@@ -31,7 +27,14 @@ public class AuraDocumentManager
 				projectName: "Test Project Name"
 			);
 			typeChecker.BuildSymbolsTable(untypedAst);
-			typeChecker.CheckTypes(untypedAst);
+			var typedAst = typeChecker.CheckTypes(untypedAst);
+
+			// Store file's new contents
+			var lspDoc = new AuraLspDocument(contents, typedAst);
+			var modDict = _documents.GetOrAdd(module, (_) => new ConcurrentDictionary<string, AuraLspDocument>());
+			modDict.AddOrUpdate(file, lspDoc, (k, v) => v);
+			_documents.AddOrUpdate(module, modDict, (k, dict) => dict);
+			_documents[module] = modDict;
 		}
 		catch (AuraExceptionContainer)
 		{
@@ -47,18 +50,18 @@ public class AuraDocumentManager
 	{
 		var (module, file) = GetModuleAndFileNames(path);
 		var modDict = _documents[module];
-		modDict.TryRemove(file, out string? _);
+		modDict.TryRemove(file, out AuraLspDocument? _);
 		if (!modDict.IsEmpty)
 		{
 			_documents[module] = modDict;
 		}
 		else
 		{
-			_documents.Remove(module, out var _);
+			_documents.Remove(module, out _);
 		}
 	}
 
-	public string? GetDocument(string path)
+	public AuraLspDocument? GetDocument(string path)
 	{
 		var (module, file) = GetModuleAndFileNames(path);
 		return _documents[module][file];
@@ -68,7 +71,7 @@ public class AuraDocumentManager
 	{
 		if (_documents.TryGetValue(module, out var value))
 		{
-			return value.Select(item => (item.Key, item.Value)).ToList();
+			return value.Select(item => (item.Key, item.Value.Contents)).ToList();
 		}
 		return new List<(string, string)>();
 	}
