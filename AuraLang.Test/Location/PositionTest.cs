0e73d7d827a43564eb41c3b63dc678664114322f
Owen Hamill <47223204+ohamill@users.noreply.github.com>
2024-02-23 23:48:32 +0100

    Add unit tests for the `Location` package (#223)

---
+++ AuraLang.Test/Location/PositionTest.cs
@@ -0,0 +1,91 @@
+﻿using AuraLang.Location;
+using MsPosition = Microsoft.VisualStudio.LanguageServer.Protocol.Position;
+
+namespace AuraLang.Test.Location;
+
+public class PositionTest
+{
+	[Test]
+	public void TestPosition_Init()
+	{
+		// Arrange
+		const int character = 5;
+		const int line = 1;
+		// Act
+		var position = new Position(character, line);
+		// Assert
+		MakeAssertions(
+			position,
+			character,
+			line
+		);
+	}
+
+	[Test]
+	public void TestPosition_Init_ZeroValues()
+	{
+		// Arrange
+		const int character = 0;
+		const int line = 0;
+		// Act
+		var position = new Position(character, line);
+		// Assert
+		MakeAssertions(
+			position,
+			character,
+			line
+		);
+	}
+
+	[Test]
+	public void TestPosition_FromMicrosoftPosition()
+	{
+		// Arrange
+		const int character = 10;
+		const int line = 3;
+		var msPosition = new MsPosition { Character = character, Line = line };
+		// Act
+		var position = Position.FromMicrosoftPosition(msPosition);
+		// Assert
+		MakeAssertions(
+			position,
+			character,
+			line
+		);
+	}
+
+	[Test]
+	public void TestPosition_Equality()
+	{
+		// Arrange
+		const int character = 5;
+		const int line = 4;
+		// Act
+		var position = new Position(character, line);
+		var other = new Position(character, line);
+		// Assert
+		Assert.That(position, Is.EqualTo(other));
+	}
+
+	[Test]
+	public void TestPosition_Inequality()
+	{
+		// Act
+		var position = new MsPosition(5, 5);
+		var other = new MsPosition(1, 1);
+		// Assert
+		Assert.That(position, Is.Not.EqualTo(other));
+	}
+
+	private void MakeAssertions(Position position, int expectedCharacter, int expectedLine)
+	{
+		Assert.Multiple(
+			() =>
+			{
+				Assert.That(position, Is.Not.Null);
+				Assert.That(position.Character, Is.EqualTo(expectedCharacter));
+				Assert.That(position.Line, Is.EqualTo(expectedLine));
+			}
+		);
+	}
+}
---
+++ AuraLang.Test/Location/RangeTest.cs
@@ -0,0 +1,96 @@
+﻿using AuraLang.Location;
+using Range = AuraLang.Location.Range;
+
+namespace AuraLang.Test.Location;
+
+public class RangeTest
+{
+	[Test]
+	public void TestRange_Init()
+	{
+		// Arrange
+		var start = new Position(5, 0);
+		var end = new Position(10, 3);
+		// Act
+		var range = new Range(start, end);
+		// Assert
+		Assert.Multiple(
+			() =>
+			{
+				Assert.That(range, Is.Not.Null);
+				Assert.That(range.Start, Is.EqualTo(start));
+				Assert.That(range.End, Is.EqualTo(end));
+			}
+		);
+	}
+
+	[Test]
+	public void TestRange_Init_ZeroValue()
+	{
+		// Arrange
+		var position = new Position(0, 0);
+		// Act
+		var range = new Range();
+		// Assert
+		Assert.Multiple(
+			() =>
+			{
+				Assert.That(range, Is.Not.Null);
+				Assert.That(range.Start, Is.EqualTo(position));
+				Assert.That(range.End, Is.EqualTo(position));
+			}
+		);
+	}
+
+	[Test]
+	public void TestRange_Contains()
+	{
+		// Arrange
+		var start = new Position();
+		var end = new Position(10, 10);
+		var position = new Position(5, 5);
+		// Act
+		var range = new Range(start, end);
+		// Assert
+		Assert.That(range.Contains(position), Is.True);
+	}
+
+	[Test]
+	public void TestRange_DoesNotContain()
+	{
+		// Arrange
+		var start = new Position();
+		var end = new Position(10, 10);
+		var position = new Position(15, 15);
+		// Act
+		var range = new Range(start, end);
+		// Assert
+		Assert.That(range.Contains(position), Is.False);
+	}
+
+	[Test]
+	public void TestRange_Contains_Start()
+	{
+		// Arrange
+		var start = new Position();
+		var end = new Position(10, 10);
+		var position = new Position();
+		// Act
+		var range = new Range(start, end);
+		// Assert
+		Assert.That(range.Contains(position), Is.True);
+	}
+
+	[Test]
+	public void TestRange_DoesNotContain_End()
+	{
+		// Arrange
+		var start = new Position();
+		var end = new Position(10, 10);
+		var position = new Position(10, 10);
+		// Act
+		var range = new Range(start, end);
+		// Assert
+		Assert.That(range.Contains(position), Is.False);
+	}
+}
