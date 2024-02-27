0da5d9aba8a7f3653c43763a6daae5d624203d8b
Owen Hamill <owen.hamill@devoteam.com>
2024-02-27 19:24:52 +0100

    Refactor hover provider

--- AuraLang.Lsp/Service/HoverProvider/AuraHoverProvider.cs
+++ AuraLang.Lsp/Service/HoverProvider/AuraHoverProvider.cs
@@ -1,5 +1,8 @@
 ﻿using AuraLang.AST;
-using AuraLang.Location;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using MsPosition = Microsoft.VisualStudio.LanguageServer.Protocol.Position;
+using Position = AuraLang.Location.Position;
+using Range = Microsoft.VisualStudio.LanguageServer.Protocol.Range;
 
 namespace AuraLang.Lsp.Service.HoverProvider;
 
@@ -8,7 +11,13 @@ namespace AuraLang.Lsp.Service.HoverProvider;
 /// </summary>
 public class AuraHoverProvider : AuraLspService
 {
-	public IHoverable? FindStmtByPosition(Position position, IEnumerable<ITypedAuraStatement> typedAst)
+	/// <summary>
+	///     Finds the statement being hovered over in the LSP client
+	/// </summary>
+	/// <param name="position">The current position of the cursor</param>
+	/// <param name="typedAst">The typed Abstract Syntax Tree representing the current Aura source file</param>
+	/// <returns>The typed AST node being hovered over, if it provides hoverable information; else null</returns>
+	private IHoverable? FindStmt(Position position, IEnumerable<ITypedAuraStatement> typedAst)
 	{
 		try
 		{
@@ -18,5 +27,37 @@ public class AuraHoverProvider : AuraLspService
 		{
 			return null;
 		}
+	}
+
+	/// <summary>
+	///     Gets the hover text of the typed AST node currently being hovered over in the LSP client
+	/// </summary>
+	/// <param name="position">The current position of the cursor</param>
+	/// <param name="typedAst">The typed Abstract Syntax Tree representing the current Aura source file</param>
+	/// <returns>
+	///     The hover text of the AST node being hovered over. If the cursor is currently hovering over a non-node (i.e.
+	///     whitespace) or the current node doesn't provide any hover text, the returned <see cref="Hover" /> object is empty
+	/// </returns>
+	public Hover GetHoverText(Position position, IEnumerable<ITypedAuraStatement> typedAst)
+	{
+		var node = FindStmt(position, typedAst);
+		if (node is null) return new Hover();
+		return new Hover
+		{
+			Contents = new MarkupContent { Value = $"```\n{node.HoverText}\n```", Kind = MarkupKind.Markdown },
+			Range = new Range
+			{
+				Start = new MsPosition
+				{
+					Character = node.HoverableRange.Start.Character,
+					Line = node.HoverableRange.Start.Line
+				},
+				End = new MsPosition
+				{
+					Character = node.HoverableRange.End.Character,
+					Line = node.HoverableRange.End.Line
+				}
+			}
+		};
 	}
 }
