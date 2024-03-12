d917b90b2e936cdeb0d9ad26889a45df97962272
Owen Hamill <owen.hamill@devoteam.com>
2024-03-12 07:46:27 +0100

    Restrict use of class's private methods

--- AuraLang/Types/IGettable.cs
+++ AuraLang/Types/IGettable.cs
@@ -9,6 +9,13 @@ public interface IGettable
 	///     Gets the type identified by the supplied attribute name
 	/// </summary>
 	/// <param name="attribute">The name of the attribute to get from this type</param>
-	/// <returns>The type of the attribute</returns>
+	/// <returns>The type of the attribute, if it exists; else null</returns>
 	AuraType? Get(string attribute);
+
+	/// <summary>
+	///     Gets the type identified by the supplied attribute name only if the identified type has public visibility
+	/// </summary>
+	/// <param name="attribute">The name of the attribute to get from this type</param>
+	/// <returns>The type of the attribute, if it exists and has public visibility; else null</returns>
+	AuraType? GetPublic(string attribute) { return Get(attribute); }
 }
